648b860deb493cda381b152bd3d196040328fdfb
Harry Fletcher <harryafletcher@gmail.com>
2022-12-17 20:40:41 +0000

    Added stuff

---
+++ Assets/Scripts/Vehicles/BasicVehicleController.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BasicVehicleController : MonoBehaviour
+{
+    [SerializeField] private float speed = 10.0f; // Speed of the vehicle
+
+    [SerializeField] private float rotationSpeed = 100.0f; // Rotation speed of the vehicle
+
+    [SerializeField] private Camera cam;
+
+    private void OnEnable()
+    {
+        print("Enabled");
+        // Setup camera to follow vehicle
+    }
+    
+    private void OnDisable()
+    {
+        print("Disabled");
+        // Stop camera following vehicle
+    }
+
+    void Update()
+    {
+        var movement = GetInput();
+        Move(movement);
+    }
+
+    private static Vector2 GetInput()
+    {
+        var movement = new Vector2();
+        
+        // Get the horizontal and vertical input from the Xbox controller
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Accelerate") - Input.GetAxis("Brake");
+
+        return movement;
+    }
+    
+    private void Move(Vector2 movement)
+    {
+        // Move the vehicle based on the input
+        transform.position += transform.forward * (movement.y * speed * Time.deltaTime);
+        
+        // Rotate the vehicle based on the input
+        transform.Rotate(Vector3.up, movement.x * rotationSpeed * Time.deltaTime * movement.y);
+    }
+}
