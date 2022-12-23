91a38e00611859847ca123ef0625f98a20f0f7ce
mikehorton279 <mikehorton279@hotmail.com>
2022-12-23 01:30:06 +0000

    FG-10 / FG-25 Created hose reel prefab with reel scaling and rotation

--- Assets/Scenes/Test Scenes/TestObiRopeExtender.cs
+++ Assets/Scenes/Test Scenes/TestObiRopeExtender.cs
@@ -10,17 +10,26 @@ public class TestObiRopeExtender : MonoBehaviour
     ObiRopeCursor cursor;
     ObiRope rope;
     Camera mainCam;
+
+    private float dist;
+    private float lastDist;
+    private float scaleValue;
+    private float hoseStartVal;
+
     public Transform hoseGun;
     public Transform baseObject;
     public Transform hoseReel;
-    private float dist;
-    private float rotAng;
     public float hoseMaxLength = 10f;
+    public float reelRotationMult = 20f;
+
 
     private void Start()
     {
         cursor = GetComponentInChildren<ObiRopeCursor>();
         rope = cursor.GetComponent<ObiRope>();
+
+        dist = Vector3.Distance(hoseGun.position, baseObject.position);
+        hoseStartVal = dist / hoseMaxLength;
 
         mainCam = Camera.main;
         if (!mainCam.gameObject.GetComponent<ObiFluidRenderer>())
@@ -32,11 +41,23 @@ public class TestObiRopeExtender : MonoBehaviour
     private void Update()
     {
         dist = Vector3.Distance(hoseGun.position, baseObject.position);
-
-        //set length of rope based on the hose gun distance from hose reel
+       
         if(dist < hoseMaxLength) 
-        { 
+        {
+            //set length of rope based on the hose gun distance from hose reel
             cursor.ChangeLength(dist);
+
+            //scale the hose reel model to simulate it getting smaller
+            scaleValue = (dist / hoseMaxLength) - hoseStartVal;
+            hoseReel.localScale = new Vector3(1, 1 - scaleValue, 1 - scaleValue);
+
+            //rotate hose reel model to simulate unwinding
+            float posDelta = dist - lastDist;
+            lastDist = dist;
+
+            float mult = hoseMaxLength * reelRotationMult;
+
+            hoseReel.Rotate(-posDelta * mult,0,0);
         }
     }
 }
